5a097e00310065a9a5d4bb6662864a7bd4b3cbfd
misael <misaelucas@gmail.com>
2023-07-29 02:53:13 -0300

    added semi-bold to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <>
       <div className="flex text-white justify-center shadow-xl pb-2">
-        <div className="mt-1 text-sm sm:text-lg xl:text-xl flex">
+        <div className="mt-1 text-sm font-semibold sm:text-lg xl:text-xl flex">
           <p className="p-1 sm:p-2 transition-transform duration-500 ease-in-out transform hover:scale-110 hover:bg-opacity-75 hover:underline underline-offset-4">
             <Link to="/contato">Contato</Link>
           </p>
